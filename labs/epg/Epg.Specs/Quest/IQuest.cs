491c7b8a4ada39b0be5eabbc523d2fb71a393204
stan <stanislav@yarmonov.pro>
2020-06-05 19:56:01 +0300

    +Quests basics.

---
+++ labs/epg/Epg.Specs/Quest/IQuest.cs
@@ -0,0 +1,22 @@
+﻿namespace Epg.Quest
+{
+    /// <summary>
+    /// Quest (adventure).
+    /// </summary>
+    ///
+    /// <remarks>
+    /// This is what we are living for.
+    /// </remarks>
+    public interface IQuest
+    {
+        /// <summary>
+        /// Quest identifier.
+        /// </summary>
+        string Id { get; }
+
+        /// <summary>
+        /// Resource description.
+        /// </summary>
+        string Description { get; }
+    }
+}
---
+++ labs/epg/Epg.Specs/Quest/QuestEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace Epg.Quest
+{
+    public class QuestEntity
+        : IQuest
+    {
+        /// <summary>
+        /// <see cref="IQuest.Id"/> implementation.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// <see cref="IQuest.Description"/> implementation.
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
